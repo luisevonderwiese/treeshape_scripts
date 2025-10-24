691f8040f680c162720c29cba99d3fab36b37f61
Luise Haeuser <haeusele@hitssv566.villa-bosch.de>
2025-10-24 12:26:08 +0200

    Make sum benchmark plots

--- scripts/benchmark_plots.py
+++ scripts/benchmark_plots.py
@@ -32,7 +32,7 @@ def linear_time(base_dirs):
 
 
 def comparison_plots(base_dirs, reference, suffix):
-    plots_dir = os.path.join("../data/general_plots", "comparing_benchmark_" + reference + suffix)
+    plots_dir = "../data/general_plots"
     if not os.path.isdir(plots_dir):
         os.makedirs(plots_dir)
     
@@ -44,37 +44,32 @@ def comparison_plots(base_dirs, reference, suffix):
         R_dir = os.path.join(base_dir, reference)
         for tree_name in util.unrooted_tree_names(base_dir):
             tree_size = sizes_df[sizes_df["tree_name"] == tree_name].iloc[0]["num_tips"]
+            if not tree_size in times_R:
+                times_R[tree_size] = [] 
+                times_python[tree_size] = []
+
+
             times_python_df = pd.read_csv(os.path.join(python_dir, tree_name + "_times" + suffix + ".tsv"), sep = "\t")
             times_R_df = pd.read_csv(os.path.join(R_dir, tree_name + "_times.tsv"), sep = "\t")
-            for index in times_R_df.columns[3:]:
-                if not index in times_R:
-                    times_R[index] = {}
-                    times_python[index] = {}
-                if not tree_size in times_R[index]:
-                    times_R[index][tree_size] = []
-                    times_python[index][tree_size] = []
-                times_R[index][tree_size] += list(times_R_df[index])
-                times_python[index][tree_size] += list(times_python_df[index])
+            relevant_indices = list(times_R_df.columns[3:])
+            times_R[tree_size] += [sum([row[index] for index in relevant_indices]) for  _, row in times_R_df.iterrows()]
+            times_python[tree_size] += [sum([row[index] for index in relevant_indices]) for  _, row in times_python_df.iterrows()]
+
     
     for times_dict in [times_R, times_python]:
-        for index, index_times in times_dict.items():
-            for size, size_times in index_times.items():
-                index_times[size] = sum(size_times) / len(size_times)
-            times_dict[index] = dict(sorted(index_times.items()))
+        for size, size_times in times_dict.items():
+            times_dict[size] = sum(size_times) / len(size_times)
+    times_R = dict(sorted(times_R.items()))
+    times_python = dict(sorted(times_python.items()))
 
-    for index, index_times_R in times_R.items():
-        print(index)
-        plt.figure(figsize=(20,20))
-        plt.plot(index_times_R.keys(), index_times_R.values(), marker = "s", label = reference)
-        index_times_python = times_python[index]
-        plt.plot(index_times_python.keys(), index_times_python.values(), marker = "s", label = "treeshapy")
-        plt.xlabel("tree size")
-        plt.ylabel("run_time")
-        util.add_fancy_legend()
-        plt.savefig(os.path.join(plots_dir, index + ".png"))
-        plt.clf()
-        plt.close()
-    print("")
+    plt.figure(figsize=(20,20))
+    plt.plot(times_R.keys(), times_R.values(), marker = "s", label = reference)
+    plt.plot(times_python.keys(), times_python.values(), marker = "s", label = "treeshapy")
+    plt.xlabel("tree size")
+    plt.ylabel("run_time (sec)")
+    util.add_fancy_legend()
+    plt.savefig(os.path.join(plots_dir, "comparing_benchmark_" + reference + suffix + ".png"))
+    plt.clf()
 
 base_dirs = ["../data/evonaps_dna"]
 
